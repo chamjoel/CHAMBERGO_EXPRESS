c252b14b45c2eddf30a87180f2531129b1e8b4d0
Joel-Chambergo <chambergo.joel@iisgalvanimi.edu.it>
2020-04-23 21:15:53 +0000

    Finito

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -1,26 +1,18 @@
 var express = require('express');
-var app = express();
-
+var app = express(); 
 const path = require('path');
-
-app.get('/', getcallback);
-function getcallback(req,res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-}
-
-app.get('/home',function(req,res){
- res.sendFile(path.join(__dirname, 'home.html'));
+app.get('/', function(req,res) {
+ res.sendFile(path.join(__dirname, 'index.html'));
+ 
 });
-
-app.get('/warning',function(req,res){
+app.get('/about', function(req,res) {
  res.sendFile(path.join(__dirname, 'about.html'));
+ 
 });
-
-app.get('/manutenzione',function(req,res){
+app.get('/sitemap', function(req,res) {
  res.sendFile(path.join(__dirname, 'sitemap.html'));
 });
 
-app.listen(3000, funzione);
-function funzione() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-}
+});
